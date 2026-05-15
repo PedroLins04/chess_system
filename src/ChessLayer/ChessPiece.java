127c5d4bdd1f22e5633f90b24192cee3867e94ea
Pedro Lins <phlins.olinda@gmail.com>
2026-05-15 10:58:26 -0400

    Possible moves of a Piece

--- src/ChessLayer/ChessPiece.java
+++ src/ChessLayer/ChessPiece.java
@@ -3,7 +3,7 @@ package ChessLayer;
 import BoardLayer.Board;
 import BoardLayer.Piece;
 
-public class ChessPiece extends Piece {
+public abstract class ChessPiece extends Piece {
 
     private Color color;
 
--- src/ChessLayer/Match.java
+++ src/ChessLayer/Match.java
@@ -30,7 +30,10 @@ public class Match {
 
     private void validateSourcePosition(Position position) {
         if (!board.thereIsAPiece(position)) {
-            throw new ChessException("There is no piece on this position");
+            throw new ChessException("There is no piece on source position");
+        }
+        if (!board.piece(position).isThereAnyPossibleMove()){
+            throw new ChessException("There is no possible moves with this Piece");
         }
     }
 
